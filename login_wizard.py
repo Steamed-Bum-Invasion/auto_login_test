0aa8d14050ba34e122984ae534e0fbe5a73f086f
Dhruv Kumarjiguda <dhruvkumarjiguda@gmail.com>
2025-08-01 12:35:17 +0530

    first commit.

--- login_wizard.py
+++ login_wizard.py
@@ -1,29 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
-# Create a new Firefox WebDriver instance
-browser = webdriver.Firefox()
 
-# Open the Firefox browser (already done)
+options = webdriver.ChromeOptions()
+
+browser = webdriver.Chrome(options=options)
 
 # Visit the specified webpage
-url = "https://www.facebook.com"
+url = "https://practicetestautomation.com/practice-test-login/"
 browser.get(url)
 
-# Input the username and password by Name attribute
-username_input = browser.find_element(By.NAME, 'email')
-password_input = browser.find_element(By.NAME, 'pass')
+# Input the username and password
+browser.find_element(By.NAME, "username").send_keys("student")
+browser.find_element(By.NAME, "password").send_keys("Password123")
 
-# Input your username and password here
-username_input.send_keys('<email-username>')
-password_input.send_keys('<password>')
+# Click the "Log in" button
+browser.find_element(By.NAME, "login").click()
 
-# Locate and click the "Log in" button by NAME attribute
-login_button = browser.find_element(By.NAME, 'login')
-login_button.click()
+# Wait for a while to observe the result (instead of an infinite loop)
+time.sleep(30)
 
-# Keep the browser open until manually closed
-while True:
-    pass  # This loop will run indefinitely until manually terminated
-
-# The browser will remain open until you manually close the script.
+# Optional: close the browser after 30 seconds
+browser.quit()
